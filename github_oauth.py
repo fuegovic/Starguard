c5275c6684745c147bde08ae20d3eb50932f0452
Marco Beretta <81851188+Berry-13@users.noreply.github.com>
2023-10-25 23:03:27 +0200

    feat: new page for successful login and invalid link

--- github_oauth.py
+++ github_oauth.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import requests
 import os
 from dotenv import load_dotenv
@@ -6,30 +6,35 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask('app')
+app.template_folder = 'html'
 
 @app.route('/')
 def home():
-  return "Hello, World!"
+    return "Hello, World!"
 
 @app.route('/callback')
 def callback():
-  code = request.args.get('code')
-  client_id = os.getenv('GITHUB_CLIENT_ID')
-  client_secret = os.getenv('GITHUB_CLIENT_SECRET')
-  data = {
-    'client_id': client_id,
-    'client_secret': client_secret,
-    'code': code
-  }
-  response = requests.post(
-    'https://github.com/login/oauth/access_token', data=data, headers={'Accept': 'application/json'})
-  access_token = response.json().get('access_token')
-  # Here you would store the access token in a database or use it to take action for the user
-  print(access_token)  # This will print the access token to the console
-  print('success')
-  print(response.json())
-  return "Authenticated successfully"
-  
+    code = request.args.get('code')
+    client_id = os.getenv('GITHUB_CLIENT_ID')
+    client_secret = os.getenv('GITHUB_CLIENT_SECRET')
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'code': code
+    }
+    response = requests.post(
+        'https://github.com/login/oauth/access_token', data=data, headers={'Accept': 'application/json'})
+    access_token = response.json().get('access_token')
+
+    if access_token:
+        # auth successful
+        message = "Authentication successful!"
+        return render_template('valid.html')
+    else:
+        # auth failed
+        message = "Invalid link!"
+        return render_template('invalidLink.html')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    
