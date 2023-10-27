01dfc3ca734279ca07da0d7b7a0d017304d2e195
Marco Beretta <81851188+Berry-13@users.noreply.github.com>
2023-10-27 17:52:34 +0200

    feat: save information to MongoDB

--- github_oauth.py
+++ github_oauth.py
@@ -3,14 +3,24 @@ import os
 from dotenv import load_dotenv
 from authlib.integrations.flask_client import OAuth
 from authlib.integrations.flask_client import OAuthError
+from pymongo import MongoClient
+from datetime import datetime, timezone
 
 load_dotenv()
 
 app = Flask('app')
 app.template_folder = 'html'
 app.secret_key = os.getenv('SECRET_KEY')
-
 oauth = OAuth(app)
+
+# Connect to the MongoDB server
+try:
+    client = MongoClient(host=os.getenv('MONGO_HOST'))
+    db = client.get_database(os.getenv('MONGO_DATABASE'))
+except Exception as e:
+    print(f"Error connecting to MongoDB: {e}")
+    raise
+
 
 github = oauth.register(
     name='github',
@@ -19,7 +29,7 @@ github = oauth.register(
     authorize_url='https://github.com/login/oauth/authorize',
     access_token_url='https://github.com/login/oauth/access_token',
     api_base_url='https://api.github.com/',
-    client_kwargs={'scope': 'user:email repo'}  # Add scope parameter here
+    client_kwargs={'scope': 'user:email repo'}
 )
 
 @app.route('/login')
@@ -29,13 +39,14 @@ def login():
 
 @app.route('/authorize')
 def authorize():
+    message = ""
     try:
         token = github.authorize_access_token()
         if token:
             # auth successful
             message = "Authentication successful!"
             print(f"Auth Status: {message}")
-            
+
             user_resp = github.get('user', token=token)
             username = user_resp.json()['login']
             print(f"User's username: {username}")
@@ -45,23 +56,44 @@ def authorize():
             print(f"User's email: {email}")
             print(f"token: {token}")
 
+            # Save user information to MongoDB
+            user_collection = db['users']
 
-            return render_template('valid.html')
+            # Check if a document with the same email exists
+            existing_user = user_collection.find_one({'email': email})
+
+            # Get the current timestamp in UTC
+            current_time = datetime.now(timezone.utc).isoformat()
+
+            user_data = {
+                'username': username,
+                'email': email,
+                'token': token,
+                'created_at': existing_user.get('created_at', current_time),
+                'updated_at': current_time,
+            }
+
+            if existing_user:
+                # Update the existing document
+                user_collection.update_one({'email': email}, {'$set': user_data})
+            else:
+                # Insert a new document
+                user_collection.insert_one(user_data)
+
         else:
             # auth failed
             message = "Invalid link!"
             print(f"Auth Status: {message}")
 
     except OAuthError as e:
-        # Handle OAuth errors here
         message = f"OAuth error: {e.description}"
         print(f"Auth Status: {message}")
-    
-    return render_template('invalidLink.html')
+
+    return render_template('result.html', message=message)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
