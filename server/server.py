1b5035ccdef4fa35bbb36c9a172d8c66be83f9fa
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2023-11-07 17:31:11 -0500

    🐞
    - Set debug to false

--- server/server.py
+++ server/server.py
@@ -134,4 +134,4 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv('SERVER_PORT'), debug=True)
+    app.run(host='0.0.0.0', port=os.getenv('SERVER_PORT'), debug=False)
